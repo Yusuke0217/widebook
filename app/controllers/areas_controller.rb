05649962afb70bd3b49dd4f6601a73cf3c51a2ce
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2020-11-27 02:34:30 -0800

    improvement area-search

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,7 @@
 class AreasController < ApplicationController
+
+  def show
+    @area = Area.find_by(id: params[:id])
+    @shops = Shop.where(area_id: @area.id)
+  end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
 
   def index
+    @areas = Area.all
   end
 end
