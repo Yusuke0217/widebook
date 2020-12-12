cc70038a8ba3cd92c860281648642916614f949f
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2020-12-12 01:39:14 -0800

    improve review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
-  # before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
-  # before_action :correct_user, only: %i[new edit update destroy]
+  before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
+  before_action :correct_user, only: %i[new edit update destroy]
 
   def index
   end
