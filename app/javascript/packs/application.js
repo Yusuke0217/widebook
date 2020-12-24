56742fe0a4618c267d07507b1e01b8cf875c75f2
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2020-12-23 21:04:23 -0800

    implement ggl_map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,13 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+// Original JavaScript at common file
+
+
+// Original JavaScript for piece file
+require("piece/ggl_map")
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
