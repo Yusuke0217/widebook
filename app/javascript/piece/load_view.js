bd5af6860531de43f49402d3d5a8b954a32f2505
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-01-16 01:05:52 -0800

    improve-load-view

--- app/javascript/piece/load_view.js
+++ app/javascript/piece/load_view.js
@@ -1,7 +1,9 @@
 'use strict';
 (() => {
-  window.onload =  () => {
-    const spin = document.getElementById('js-loading');
+  window.addEventListener('load', () => {
+    let spin = document.getElementById('js-loading');
+    spin.classList.remove('load__wrapper');
     spin.classList.add('loaded');
-  };
+  });
+  
 })();
