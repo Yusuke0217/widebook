d3e83dc2698432457959316f4114aabc5cb999a3
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-01-24 23:22:51 -0800

    js-prefix

--- app/javascript/piece/load_view.js
+++ app/javascript/piece/load_view.js
@@ -1,11 +1,11 @@
 (() => {
-  // document.addEventListener('turbolinks:load', () => {
-  // });
-  window.addEventListener('load', () => {
+  document.addEventListener('turbolinks:load', () => {
     'use strict';
     let spin = document.getElementById('js-loading');
     spin.classList.remove('load__wrapper');
     spin.classList.add('loaded');
   });
+  // window.addEventListener('load', () => {
+  // });
   
 })();
