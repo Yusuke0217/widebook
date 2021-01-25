ca2aa6badde06b1d82ed1c4754c911793104050e
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-01-24 17:53:08 -0800

    implement-js-scroll

--- app/javascript/piece/move_btn.js
+++ app/javascript/piece/move_btn.js
@@ -1,16 +1,28 @@
 (function() {
   'use strict';
   document.addEventListener('DOMContentLoaded', () => {
-    let $prev = document.querySelector('.js-prev');
-    let $next = document.querySelector('.js-next');
-    let $ul = document.getElementById('js-slide-li');
+    let $prev = document.querySelectorAll('.js-prev');
+    let $next = document.querySelectorAll('.js-next');
+    let $contents = document.getElementById('js-content-li');
+    let $shops = document.getElementById('js-shop-li');
     
-    $prev.addEventListener('click', () => {
-      $ul.scrollBy({top: 0, left: -145, behavior: "smooth"});
+    $prev[0].addEventListener('click', () => {
+      $contents.scrollBy({top: 0, left: -145, behavior: "smooth"});
     });
-  
-    $next.addEventListener('click', () => {
-      $ul.scrollBy({top: 0, left: 145, behavior: "smooth"});
+    
+    $prev[1].addEventListener('click', () => {
+      $shops.scrollBy({top: 0, left: -260, behavior: "smooth"});
     });
+
+
+    $next[0].addEventListener('click', () => {
+      $contents.scrollBy({top: 0, left: 145, behavior: "smooth"});
+
+    });
+
+    $next[1].addEventListener('click', () => {
+      $shops.scrollBy({top: 0, left: 260, behavior: "smooth"});
+    });
+    
   });
 })();
