2add5994df987a707b8b647a16308783c7fca6e4
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-01-27 17:39:47 -0800

    fix-img

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,9 +6,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
 
   if Rails.env.production?
-    storage: fog
+    storage :fog
   else
-    strage: file
+    strage :file
   end
 
   # storage :file
