0b453e73d7bc2e920da4ba3dbdd0e0b20506b542
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-02-25 23:47:01 -0800

    index_panel_design

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,6 @@
 class ShopsController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
-  before_action :admin_user, except: [:index, :show]
+  before_action :admin_user, only: [:new, :edit, :create, :update, :destroy]
   before_action :find_shop, only: [:edit, :update, :show, :destroy]
 
   def index
