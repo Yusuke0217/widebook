d14a9d3a05c0dde6b73b7759c92a0c1e652862cb
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-03-18 20:05:31 -0700

    header-refact

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,6 +5,5 @@ class HomesController < ApplicationController
     @categories = Category.all.page(params[:page]).per(15)
     @purposes = Purpose.all
     @shops = Shop.all
-    @user = User.find_by(id: current_user.id)
   end
 end
