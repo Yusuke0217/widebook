ef5202b33cd60cfb4a0b34f1b87f1c167bed31a0
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-03-18 19:10:42 -0700

    owner-menu-up

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,7 @@
 class MenusController < ApplicationController
-  before_action :logged_in_user, only: [:new, :edit, :create, :destroy, :update]
+
   before_action :logged_in_owner, only: [:new, :edit, :create, :destroy, :update]
-  before_action :admin_user, only: [:new, :edit, :create, :destroy, :update]
+  # before_action :admin_user, only: [:new, :edit, :create, :destroy, :update]
   before_action :find_shop, only: [:new, :create]
 
   def show
