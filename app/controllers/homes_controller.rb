1fc6a491569327c20fc86d5893b4fea3bbdf8304
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2021-03-18 20:03:04 -0700

    improve-header

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,5 +5,6 @@ class HomesController < ApplicationController
     @categories = Category.all.page(params[:page]).per(15)
     @purposes = Purpose.all
     @shops = Shop.all
+    @user = User.find_by(id: current_user.id)
   end
 end
